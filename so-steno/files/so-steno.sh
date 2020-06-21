9bb4232f1f719ebcfd8a2b33fae1a7f0a8c8fa31
Jason Ertel <jason.ertel@securityonionsolutions.com>
2020-06-21 08:31:07 -0400

    Remove Sensoroni executable from stenographer image

--- so-steno/files/so-steno.sh
+++ so-steno/files/so-steno.sh
@@ -5,6 +5,4 @@
 
 chown -R 941:941 /etc/stenographer/certs
 
-#runuser -l stenographer -c '/opt/sensoroni/sensoroni -config /opt/sensoroni/sensoroni.json && /usr/bin/stenographer -syslog=false >> /var/log/stenographer/stenographer.log 2>&1'
-runuser -l stenographer -c '/opt/sensoroni/sensoroni -c /opt/sensoroni/sensoroni.json &'
 runuser -l stenographer -c '/usr/bin/stenographer -syslog=false >> /var/log/stenographer/stenographer.log 2>&1' 
