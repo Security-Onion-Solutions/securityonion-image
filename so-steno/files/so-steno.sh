7fffd0a6811fc4708286cb60b9eee17c206be1ff
Jason Ertel <jason.ertel@securityonionsolutions.com>
2020-06-21 15:47:27 -0400

    Ensure certs are group readable by socore so that Sensoroni process can read the client key

--- so-steno/files/so-steno.sh
+++ so-steno/files/so-steno.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Generate the keys if they have not been already
-/usr/bin/stenokeys.sh 941 941
+/usr/bin/stenokeys.sh 941 939
 
-chown -R 941:941 /etc/stenographer/certs
+chown -R 941:939 /etc/stenographer/certs
 
 runuser -l stenographer -c '/usr/bin/stenographer -syslog=false >> /var/log/stenographer/stenographer.log 2>&1' 
