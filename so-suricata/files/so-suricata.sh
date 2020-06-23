db8481aa6bc63115f211acef41c078a93750295c
Jason Ertel <jason.ertel@securityonionsolutions.com>
2020-06-23 18:14:45 -0400

    Allow custom parameters to be specified to Suri process

--- so-suricata/files/so-suricata.sh
+++ so-suricata/files/so-suricata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Start Suricata - --init-errors-fatal could be added to make it die if rules are wrong
-/opt/suricata/bin/suricata -c /etc/suricata/suricata.yaml --af-packet=$INTERFACE --user=940 --group=940 -F /etc/suricata/bpf
+/opt/suricata/bin/suricata -c /etc/suricata/suricata.yaml --af-packet=$INTERFACE --user=940 --group=940 -F /etc/suricata/bpf $@
