b5f5b95d709f5a613642a458c0f5c5443b76d081
Mike Reeves <luke@geekempire.com>
2020-08-20 16:09:30 -0400

    Add pidfile declaration

--- so-suricata/files/so-suricata.sh
+++ so-suricata/files/so-suricata.sh
@@ -6,4 +6,4 @@ if [ -n "$INTERFACE" ]; then
 fi
 
 # Start Suricata - --init-errors-fatal could be added to make it die if rules are wrong
-/opt/suricata/bin/suricata -c /etc/suricata/suricata.yaml $AFPACKET --user=940 --group=940 -F /etc/suricata/bpf $@
+/opt/suricata/bin/suricata -c /etc/suricata/suricata.yaml $AFPACKET --user=940 --group=940 --pidfile /var/run/suricata.pid -F /etc/suricata/bpf $@
