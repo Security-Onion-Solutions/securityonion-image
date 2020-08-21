5b7175539ce45132e8b7e013270fb7075cb33372
Mike Reeves <luke@geekempire.com>
2020-08-21 12:48:36 -0400

    remove pid before restart

--- so-suricata/files/so-suricata.sh
+++ so-suricata/files/so-suricata.sh
@@ -4,6 +4,7 @@ AFPACKET=
 if [ -n "$INTERFACE" ]; then
   AFPACKET=--af-packet=$INTERFACE
 fi
-
+# delete the old PID do Suricata will start
+rm -rf /var/run/suricata.pid
 # Start Suricata - --init-errors-fatal could be added to make it die if rules are wrong
-/opt/suricata/bin/suricata -c /etc/suricata/suricata.yaml $AFPACKET --user=940 --group=940 --pidfile /var/run/suricata/suricata.pid -F /etc/suricata/bpf $@
+/opt/suricata/bin/suricata -c /etc/suricata/suricata.yaml $AFPACKET --user=940 --group=940 --pidfile /var/run/suricata.pid -F /etc/suricata/bpf $@
