db94a15ef4c67ca7785886eaffd53351d6702bb4
Josh Brower <Josh@Defensivedepth.com>
2020-08-26 05:10:55 -0400

    Playbook - Update SigmaTools & add .security config

--- so-soctopus/so-soctopus/playbook.py
+++ so-soctopus/so-soctopus/playbook.py
@@ -296,8 +296,8 @@ def sigmac_generate(sigma):
     print(sigma, file=temp_file)
     temp_file.seek(0)
 
-    sigmac_output = subprocess.run(["sigmac", "-t", "es-qs", temp_file.name, "-c", "playbook/sysmon.yml", "-c",
-                                    "playbook/securityonion-network.yml", "-c", "playbook/securityonion-baseline.yml"],
+    sigmac_output = subprocess.run(["sigmac", "-t", "es-qs", "-c", "playbook/sysmon.yml",
+                                    "-c", "playbook/securityonion-baseline.yml", "--backend-option", "keyword_field=.keyword", "--backend-option", "analyzed_sub_field_name=.security", "--backend-option", "wildcard_use_keyword=false", temp_file.name],
                                    stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='ascii')
 
     es_query = sigmac_output.stdout.strip() + sigmac_output.stderr.strip()
@@ -314,9 +314,9 @@ def sigma_metadata(sigma_raw, sigma, play_id):
 
     product = sigma['logsource']['product'] if 'product' in sigma['logsource'] else 'none'
 
-    esquery = subprocess.run(["sigmac", "-t", "elastalert", temp_file.name, "-c", "playbook/sysmon.yml", "-c",
-                              "playbook/securityonion-network.yml", "-c", "playbook/securityonion-baseline.yml"],
-                             stdout=subprocess.PIPE, encoding='ascii')
+    esquery = subprocess.run(["sigmac", "-t", "elastalert", "-c", "playbook/sysmon.yml",
+                                    "-c", "playbook/securityonion-baseline.yml", "--backend-option", "keyword_field=.keyword", "--backend-option", "analyzed_sub_field_name=.security", "--backend-option", "wildcard_use_keyword=false", temp_file.name],
+                                   stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='ascii')
 
     ea_config = re.sub(r'alert:\n.*filter:\n', 'filter:\n', esquery.stdout.strip(), flags=re.S)
 
