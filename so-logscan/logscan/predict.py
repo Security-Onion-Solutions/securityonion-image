b407a59f89614a67fed2a86f1707292fbc4cdd6a
Sagar Singhal <sagar.singhal@securityonionsolutions.com>
2021-06-09 14:45:56 -0400

    Fixes time sorting of alerts in predict.py

--- so-logscan/logscan/predict.py
+++ so-logscan/logscan/predict.py
@@ -17,24 +17,25 @@ def produce_alerts(model, dataset, split_data):
         y = predict(model, [dataset[i]])
         if y >= 0.5:
             source_ip = split_data[i][0][2]
-            start_time = dt.datetime.fromtimestamp(split_data[i][0][1]).astimezone()
-            end_time = dt.datetime.fromtimestamp(split_data[i][-1][1]).astimezone()
+            start_timestamp = dt.datetime.fromtimestamp(split_data[i][0][1]).astimezone()
+            end_timestamp = dt.datetime.fromtimestamp(split_data[i][-1][1]).astimezone()
 
-            tz_string = str(start_time.tzinfo.tzname(start_time))
+            tz_string = str(start_timestamp.tzinfo.tzname(start_timestamp))
 
-            start_time = start_time.strftime("%m-%d-%Y %-l:%M%p ") + tz_string
-            end_time = end_time.strftime("%m-%d-%Y %-l:%M%p ") + tz_string
+            start_time = start_timestamp.strftime("%m-%d-%Y %-l:%M%p ") + tz_string
+            end_time = end_timestamp.strftime("%m-%d-%Y %-l:%M%p ") + tz_string
             
             num_attempts = str(len(split_data[i]))
             num_failed = str(dataset[i][1])
             
-            alert_data.append([source_ip, start_time, end_time, num_attempts, num_failed])
+            alert_data.append([start_timestamp, source_ip, start_time, end_time, num_attempts, num_failed])
         
+    alert_data.sort()
     return alert_data
 
 
 def write_json_alerts(alert_data, outfile_path):
-    lines = [json.dumps({'Source IP': i[0], 'Start Time': i[1], 'End Time': i[2], 'Number of Login Attempts': i[3], 'Number of Failed Login Attempts': i[4]}) for i in alert_data]
+    lines = [json.dumps({'Source IP': i[1], 'Start Time': i[2], 'End Time': i[3], 'Number of Login Attempts': i[4], 'Number of Failed Login Attempts': i[5]}) for i in alert_data]
     with open(outfile_path, 'w') as outfile:
         [outfile.write(line + '\n') for line in lines]
     
