2647872f03ddf1838dd2e0371f4f4d38d7ea2410
William Wernert <william.wernert@securityonionsolutions.com>
2021-07-22 14:08:20 -0400

    Update readme, move dev requirements to their own file + update setup.py

--- so-logscan/setup.py
+++ so-logscan/setup.py
@@ -10,6 +10,10 @@ with open(f'{HERE}/README.md') as f:
 with open(f'{HERE}/requirements.txt') as f:
   required = f.read().splitlines()
 
+with open(f'{HERE}/requirements-dev.txt') as f:
+  dev_required = f.read().splitlines()
+  dev_required = dev_required[1:]
+
 setup(
   name='logscan',
   version='0.0.1',
@@ -22,6 +26,9 @@ setup(
   package_dir={'':'src'},
   include_package_data=True,
   install_requires=required,
+  extras_require={
+    'dev': dev_required,
+  },
   entry_points= {
     'console_scripts': [
       'so-logscan=logscan.run:main'
