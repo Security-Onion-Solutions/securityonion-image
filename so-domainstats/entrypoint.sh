a016b33f183375f0e5bb16b9520555d4854e4884
Mike Reeves <mike.reeves@securityonionsolutions.com>
2022-09-07 09:45:25 -0400

    Remove unwanted repos

--- so-domainstats/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd /opt/domain_stats || exit
-python3 domain_stats.py -ip 0.0.0.0 20000 -a top-1m.csv --preload 0
--- so-freqserver/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd opt/freq_server/freq || exit
-python freq_server.py -s 0 -ip 0.0.0.0 10004 freqtable2018.freq
