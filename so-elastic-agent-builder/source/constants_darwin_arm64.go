7ddc069fa247e74e3c2ec41b26bf968b5503629d
Josh Brower <josh@defensivedepth.com>
2023-05-12 12:10:56 -0400

    Add darwin arm64

--- so-elastic-agent-builder/source/constants_darwin_arm64.go
+++ so-elastic-agent-builder/source/constants_darwin_arm64.go
@@ -4,7 +4,7 @@ import (
 	_ "embed"
 )
 
-// //go:embed files/elastic-agent/darwin-arm64.tar.gz
+//go:embed files/elastic-agent/darwin-aarch64.tar.gz
 var agentFiles []byte
 
 const installPath = "/Library/Elastic/SO/"
