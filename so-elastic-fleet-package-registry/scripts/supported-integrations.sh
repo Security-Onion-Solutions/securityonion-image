f97ca608b8fe22ab3ebf03f24aa36f1f95e845b2
defensivedepth <josh@defensivedepth.com>
2023-10-17 15:09:35 -0400

    Cleanup

--- so-elastic-fleet-package-registry/scripts/supported-integrations.sh
+++ so-elastic-fleet-package-registry/scripts/supported-integrations.sh
@@ -1,4 +1,12 @@
-#/bin/bash
+#!/bin/bash
+#
+# Copyright Security Onion Solutions LLC and/or licensed to Security Onion Solutions LLC under one
+# or more contributor license agreements. Licensed under the Elastic License 2.0 as shown at
+# https://securityonion.net/license; you may not use this file except in compliance with the
+# Elastic License 2.0.
+
+# This script removes unsupported packages from our self-hosted Elastic Package Repo container image.
+# It is meant to be used during the image build process.
 
 cd /packages/package-storage/
 for file in *
