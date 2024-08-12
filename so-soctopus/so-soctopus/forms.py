83694433703b46b975455065cf395e6363cc6cec
Josh Brower <josh@defensivedepth.com>
2024-08-12 07:56:19 -0400

    Delete so-soctopus directory

--- so-soctopus/so-soctopus/forms.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import StringField
-
-
-class DefaultForm(FlaskForm):
-   esindex = StringField('esindex')
-   esid = StringField('esid')
