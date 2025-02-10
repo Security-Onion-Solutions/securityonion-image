54daa3e5bcdc71fb03d546c1e770b1090f4b3da5
reyesj2 <94730068+reyesj2@users.noreply.github.com>
2025-02-10 15:30:56 -0600

    support all EA compatiable integrations
    Signed-off-by: reyesj2 <94730068+reyesj2@users.noreply.github.com>

--- so-elastic-fleet-package-registry/scripts/supported-integrations.sh
+++ so-elastic-fleet-package-registry/scripts/supported-integrations.sh
@@ -11,7 +11,7 @@
 cd /packages/package-storage/
 for file in *
 do
-    PATTERN=$(echo $file | cut -d "-" -f 1)-
+    PATTERN=$(echo $file | cut -d "-" -f 1)
     [[ ! $(grep -x "$PATTERN" /scripts/supported-integrations.txt) ]] && rm "$file" && echo "Deleted: $file..."
 done
 
