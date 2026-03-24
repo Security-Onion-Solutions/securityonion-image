6213b04363aec825a0eb0e17fcfe22192c5e9aad
Mike Reeves <reevesmk@gmail.com>
2026-03-23 20:07:15 -0400

    run zkg install as zeek user to preserve file ownership

--- so-zeek/files/zeek.sh
+++ so-zeek/files/zeek.sh
@@ -9,8 +9,8 @@ if [ -d "$CUSTOM_PKG_DIR" ]; then
   for pkg in "$CUSTOM_PKG_DIR"/*/; do
     [ -d "$pkg" ] || continue
     echo "Installing custom Zeek package: $pkg"
-    git config --global --add safe.directory "$pkg"
-    /opt/zeek/bin/zkg install --force --skiptests "$pkg"
+    runuser zeek -c "git config --global --add safe.directory \"$pkg\""
+    runuser zeek -c "/opt/zeek/bin/zkg install --force --skiptests \"$pkg\""
   done
 fi
 
